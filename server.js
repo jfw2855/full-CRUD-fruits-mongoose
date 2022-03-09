c671dc7ee5bdf0ce1401ee571a90f8a7f5777263
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-09 10:47:36 -0600

    adds mongoose connection and fruit model

--- server.js
+++ server.js
@@ -1,47 +1,64 @@
-/////////////////////////////////////////////
+/////////////////////////////////
 // import dependencies
-/////////////////////////////////////////////
-
-//this allows us to load our env variables
+/////////////////////////////////
+// this allows us to load our env variables
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-// we'll also import our fruits model when we have it
+const Fruit = require('./models/fruit')
 
-
-/////////////////////////////////////////////
+////////////////////////////////////////////
 // Create our express application object
-/////////////////////////////////////////////
+////////////////////////////////////////////
 const app = require('liquid-express-views')(express())
 
-/////////////////////////////////////////////
+////////////////////////////////////////////
 // Middleware
-/////////////////////////////////////////////
-
-//this is for request logging
+////////////////////////////////////////////
+// this is for request logging
 app.use(morgan('tiny'))
 app.use(methodOverride('_method'))
-//parses urlencoded request bodies
-app.use(express.urlencoded({extended: false}))
-//to server files from public statically 
+// parses urlencoded request bodies
+app.use(express.urlencoded({ extended: false }))
+// to serve files from public statically
 app.use(express.static('public'))
 
-/////////////////////////////////////////////
+////////////////////////////////////////////
 // Routes
-/////////////////////////////////////////////
-app.get('/', (req,res)=> {
+////////////////////////////////////////////
+app.get('/', (req, res) => {
+    console.log('hello')
     res.send('your server is running, better go catch it')
 })
 
 
+app.get('/fruits/seed', (req,res) => {
+    // arr of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false },
+      ]
+    
+    // when we see data, there are a few steps involved 
+    // delete all the data that already exists (will only happen if data exists)
+    Fruit.remove({})
+      .then(data => {
+          console.log('this is what create returns',data)
+      })
+    // Then we create with our seed data
+    // Then we can send if we want to see that data
+})
 
 
 
-/////////////////////////////////////////////
+////////////////////////////////////////////
 // Server Listener
-/////////////////////////////////////////////
+////////////////////////////////////////////
 const PORT = process.env.PORT
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`app is listening on port: ${PORT}`)
 })
